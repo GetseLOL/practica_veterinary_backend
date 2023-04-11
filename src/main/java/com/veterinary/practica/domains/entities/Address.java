3dc0f7e33f5945e9522861529f5d99f9c3b8a853
Getsemani <getsemanilol29@gmail.com>
2023-04-11 16:13:15 -0700

    creación entity address

--- src/main/java/com/veterinary/practica/domains/entities/Address.java
+++ src/main/java/com/veterinary/practica/domains/entities/Address.java
@@ -1,2 +1,32 @@
-package com.veterinary.practica.domains.entities;public class Address {
+package com.veterinary.practica.domains.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "address")
+public class Address {
+    @Id
+    @Column(name = "id_address")
+    private Long id;
+    @Column(nullable = false)
+    private Long idStreet;
+    @Column(length = 250)
+    private String reference;
+    @Column(length = 25, nullable = false)
+    private String postalCode;
+    @Column(length = 25, nullable = false)
+    private String numInt;
+    @Column(length = 25)
+    private String numExt;
+
 }
