970859cde50181f25c3323bc0f40338e4166544a
Getsemani <getsemanilol29@gmail.com>
2023-04-11 16:15:49 -0700

    implementando commandLinerunner para probar los repositories

--- src/main/java/com/veterinary/practica/VeterinaryApplication.java
+++ src/main/java/com/veterinary/practica/VeterinaryApplication.java
@@ -1,13 +1,18 @@
 package com.veterinary.practica;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VeterinaryApplication {
+public class VeterinaryApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(VeterinaryApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        
+    }
 }
