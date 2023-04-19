4cf8b4d5c958d9b01f7ae4f34958cc24e91e9910
Getsemani <getsemanilol29@gmail.com>
2023-04-19 00:08:55 -0700

    creando archivo y agregando anotaciones basicas de la clase

--- src/main/java/com/veterinary/practica/api/controllers/address/StateCountryRestController.java
+++ src/main/java/com/veterinary/practica/api/controllers/address/StateCountryRestController.java
@@ -1,2 +1,10 @@
-package com.veterinary.practica.api.controllers.address;public class StateCountryRestController {
-}
+package com.veterinary.practica.api.controllers.address;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("address/state_country")
+@AllArgsConstructor
+public class StateCountryRestController{}
