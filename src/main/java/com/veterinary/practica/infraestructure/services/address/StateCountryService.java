1084359fc6e397bb7889b7fc77477e2e659b59f5
Getsemani <getsemanilol29@gmail.com>
2023-04-19 00:21:28 -0700

    terminando metodo create

--- src/main/java/com/veterinary/practica/infraestructure/services/address/StateCountryService.java
+++ src/main/java/com/veterinary/practica/infraestructure/services/address/StateCountryService.java
@@ -1,10 +1,15 @@
 package com.veterinary.practica.infraestructure.services.address;
 
 import com.veterinary.practica.api.models.requests.address.StateCountryRequest;
+import com.veterinary.practica.api.models.responses.address.CountryResponse;
 import com.veterinary.practica.api.models.responses.address.StateCountryResponse;
+import com.veterinary.practica.domains.entities.address.StateCountryEntity;
+import com.veterinary.practica.domains.repositories.address.CountryRepository;
+import com.veterinary.practica.domains.repositories.address.StateCountryRepository;
 import com.veterinary.practica.infraestructure.abstract_services.address.IStateCountryService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,30 +18,49 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Service
 public class StateCountryService implements IStateCountryService{
+
+	private final CountryRepository countryRepository;
+	private final StateCountryRepository stateCountryRepository;
+
 	@Override
 	public StateCountryResponse create(StateCountryRequest request){
+		var byIdCountry = countryRepository.findById(request.getByIdCountry()).orElseThrow();
+
+		var built = StateCountryEntity.builder()
+		                              .name(request.getName())
+		                              .country(byIdCountry)
+		                              .build();
+		var saved = stateCountryRepository.save(built);
+		log.info("State Country saved with id: {}", saved.getId());
+		return entityToResponse(saved);
+	}
+
+	@Override
+	public StateCountryResponse update(StateCountryRequest request, Long id){
 		return null;
 	}
 
 	@Override
-	public StateCountryResponse update(
-			StateCountryRequest request,
-			Long aLong){
-		return null;
-	}
-
-	@Override
-	public void delete(Long aLong){
+	public void delete(Long id){
 
 	}
 
 	@Override
-	public StateCountryResponse read(Long aLong){
+	public StateCountryResponse read(Long id){
 		return null;
 	}
 
 	@Override
 	public Iterable<StateCountryResponse> readAll(){
 		return null;
+	}
+
+	private StateCountryResponse entityToResponse(StateCountryEntity entity){
+		var response = new StateCountryResponse();
+		BeanUtils.copyProperties(entity, response);
+		var countryResponse = new CountryResponse();
+		BeanUtils.copyProperties(entity.getCountry(), countryResponse);
+		response.setCountry(countryResponse);
+		return response;
 	}
 }
