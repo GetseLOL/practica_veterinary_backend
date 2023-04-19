e516ab4355ed19e9dc5fb264d504155b013a7901
Getsemani <getsemanilol29@gmail.com>
2023-04-19 00:24:30 -0700

    implementando metodo post

--- src/main/java/com/veterinary/practica/api/controllers/address/StateCountryRestController.java
+++ src/main/java/com/veterinary/practica/api/controllers/address/StateCountryRestController.java
@@ -1,6 +1,12 @@
 package com.veterinary.practica.api.controllers.address;
 
+import com.veterinary.practica.api.models.requests.address.StateCountryRequest;
+import com.veterinary.practica.api.models.responses.address.StateCountryResponse;
+import com.veterinary.practica.infraestructure.abstract_services.address.IStateCountryService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("address/state_country")
 @AllArgsConstructor
 public class StateCountryRestController{
+
+	private final IStateCountryService countryService;
+
+	@PostMapping
+	public ResponseEntity<StateCountryResponse> post(@RequestBody StateCountryRequest request){
+		return ResponseEntity.ok(this.countryService.create(request));
+	}
 
 }
