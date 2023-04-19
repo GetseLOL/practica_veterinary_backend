ec56fdc6c053ff7ce7f13b210cadaa3dc0ecaa0b
Getsemani <getsemanilol29@gmail.com>
2023-04-19 00:25:29 -0700

    agregando anotacion valid

--- src/main/java/com/veterinary/practica/api/controllers/address/StateCountryRestController.java
+++ src/main/java/com/veterinary/practica/api/controllers/address/StateCountryRestController.java
@@ -3,6 +3,7 @@ package com.veterinary.practica.api.controllers.address;
 import com.veterinary.practica.api.models.requests.address.StateCountryRequest;
 import com.veterinary.practica.api.models.responses.address.StateCountryResponse;
 import com.veterinary.practica.infraestructure.abstract_services.address.IStateCountryService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class StateCountryRestController{
 	private final IStateCountryService countryService;
 
 	@PostMapping
-	public ResponseEntity<StateCountryResponse> post(@RequestBody StateCountryRequest request){
+	public ResponseEntity<StateCountryResponse> post(@RequestBody @Valid StateCountryRequest request){
 		return ResponseEntity.ok(this.countryService.create(request));
 	}
 
