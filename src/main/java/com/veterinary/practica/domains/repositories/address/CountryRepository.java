a8631e8b896849bcb62caaaad2efa52c89201761
Getsemani <getsemanilol29@gmail.com>
2023-04-18 19:36:03 -0700

    agregando ordenamiento por id

--- src/main/java/com/veterinary/practica/domains/repositories/address/CountryRepository.java
+++ src/main/java/com/veterinary/practica/domains/repositories/address/CountryRepository.java
@@ -3,5 +3,8 @@ package com.veterinary.practica.domains.repositories.address;
 import com.veterinary.practica.domains.entities.address.CountryEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CountryRepository extends CrudRepository<CountryEntity, Integer> {
+	List<CountryEntity> findAllByOrderByIdAsc();
 }
