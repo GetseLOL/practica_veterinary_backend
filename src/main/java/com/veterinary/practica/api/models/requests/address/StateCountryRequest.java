98e456896fed4ef680cd28ac34737874d582ea7c
Getsemani <getsemanilol29@gmail.com>
2023-04-19 00:34:47 -0700

    agregando validacion statecountryrequest

--- src/main/java/com/veterinary/practica/api/models/requests/address/StateCountryRequest.java
+++ src/main/java/com/veterinary/practica/api/models/requests/address/StateCountryRequest.java
@@ -1,15 +1,23 @@
 package com.veterinary.practica.api.models.requests.address;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class StateCountryRequest {
+	@Length(min = 4, max = 100, message = "The size have to a between 4 and 100 characters")
+	@NotBlank(message = "The name is mandatory")
 	private String name;
+	@Positive(message = "The number is positive required")
+	@Min(value = 1)
+	@Digits(integer = 20, fraction = 0, message = "The number is mandatory")
+	@NotNull(message = "The id is mandatory")
 	private Integer byIdCountry;
 }
