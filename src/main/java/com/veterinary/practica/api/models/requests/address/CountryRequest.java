bb1acae912597417456bfe2fb98ab2128e15811c
Getsemani <getsemanilol29@gmail.com>
2023-04-18 19:35:42 -0700

    configuracion y validacion de countryrequest

--- src/main/java/com/veterinary/practica/api/models/requests/address/CountryRequest.java
+++ src/main/java/com/veterinary/practica/api/models/requests/address/CountryRequest.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CountryRequest {
+	@Length(min = 4, max = 50, message = "The size have to a between 4 and 50  characters")
 	@NotBlank(message = "Name is mandatory")
 	private String name;
 }
