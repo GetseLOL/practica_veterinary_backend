633a578d6c282b4dd861ea5909ea6abffa301fa6
Getsemani <getsemanilol29@gmail.com>
2023-04-18 19:39:24 -0700

    creando generico catalogo crud

--- src/main/java/com/veterinary/practica/infraestructure/abstract_services/shared/CrudCatalogService.java
+++ src/main/java/com/veterinary/practica/infraestructure/abstract_services/shared/CrudCatalogService.java
@@ -1,2 +1,18 @@
-package com.veterinary.practica.infraestructure.abstract_services.shared;public interface CrudCatalogService {
+package com.veterinary.practica.infraestructure.abstract_services.shared;
+
+import com.veterinary.practica.utils.enums.SortType;
+import org.springframework.data.domain.Page;
+
+public interface CrudCatalogService<RQ, RS, ID> {
+	RS create(RQ request);
+
+	RS update(RQ request, ID id);
+
+	void delete(ID id);
+
+	RS read(ID id);
+
+	RS readAll();
+
+	Page<RS> readAll(Integer page, Integer size, SortType sortType);
 }
