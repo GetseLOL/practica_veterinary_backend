c8a86eb2bdac4495721c3b28b429934716da43b7
Getsemani <getsemanilol29@gmail.com>
2023-05-02 11:25:22 -0700

    cambiando nobmre de archivo

---
+++ src/main/java/com/veterinary/practica/api/models/requests/address/ColonyRequest.java
@@ -0,0 +1,16 @@
+package com.veterinary.practica.api.models.requests.address;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ColonyRequest {
+	private Long id;
+	private String name;
+	private MunicipalityRequest municipality;
+}
